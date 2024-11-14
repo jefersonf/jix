96b6fd8c8b5ea1a8429ae3cbfb6164c3cf8fcae2
jeferson <jefersonnpn@gmail.com>
2024-11-14 13:24:22 -0300

    :sparkles: add saveToCSVFile impl

--- cmd/persistence.go
+++ cmd/persistence.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"encoding/csv"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -15,47 +16,77 @@ func saveToFile(issues []jira.Issue) {
 	verboseLog("%v items extracted\n", len(issues))
 	verboseLog("saving %s issues into %s folder\n", projectKey, outputPath)
 
+	_, err := createOutputDir(outputPath)
+	if err != nil {
+		log.Println("Error creating output dir:", err)
+		return
+	}
+
 	if outputFormat == "jsonl" {
 		saveToJSONLFile(issues)
-	} else {
+	} else if outputFormat == "csv" {
 		saveToCSVFile(issues)
+	} else {
+		log.Fatalln("unsupported output format")
 	}
 }
 
 func saveToCSVFile(issues []jira.Issue) {
-	panic("unimplemented")
+	csvFilePath := strings.ToLower(fmt.Sprintf("%s/%s.csv", outputPath, projectKey))
+
+	file, err := os.Create(csvFilePath)
+	if err != nil {
+		log.Println("Error creating file:", err)
+		return
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	writer.Write([]string{"ID", "Key", "Summary", "StatusDescription", "StatusName"})
+
+	for _, issue := range issues {
+		writer.Write([]string{
+			issue.ID,
+			issue.Key,
+			issue.Fields.Summary,
+			issue.Fields.Status.Description,
+			issue.Fields.Status.Name,
+		})
+	}
+
+	verboseLog("CSV data has been written to %s\n", csvFilePath)
 }
 
 func saveToJSONLFile(issues []jira.Issue) {
 
 	jsonFilePath := strings.ToLower(fmt.Sprintf("%s/%s.jsonl", outputPath, projectKey))
 
-	file, err := createOutputDir(jsonFilePath)
+	file, err := os.Create(jsonFilePath)
 	if err != nil {
-		fmt.Println("Error creating file:", err)
+		log.Println("Error creating file:", err)
 		return
 	}
 	defer file.Close()
 
 	for _, issue := range issues {
-		writeIssueToFile(file, &issue)
+		jsonData, err := json.Marshal(issue)
+		if err != nil {
+			log.Println("Error marshalling issue to JSON:", err)
+		} else {
+			jsonData = append(jsonData, byte('\n'))
+			writeIssueToFile(file, jsonData)
+		}
 	}
 
 	verboseLog("JSONL data has been written to %s\n", jsonFilePath)
 }
 
-func writeIssueToFile(file *os.File, issue *jira.Issue) {
-	jsonData, err := json.Marshal(*issue)
-	if err != nil {
-		log.Println("Error marshalling issue to JSON:", err)
-		return
-	}
-
-	jsonData = append(jsonData, byte('\n'))
-	_, err = file.Write(jsonData)
+func writeIssueToFile(file *os.File, data []byte) {
+	_, err := file.Write(data)
 	if err != nil {
 		log.Println("Error writing to file:", err)
-		return
 	}
 }
 
@@ -63,5 +94,5 @@ func createOutputDir(outputPath string) (*os.File, error) {
 	if err := os.MkdirAll(filepath.Dir(outputPath), 0770); err != nil {
 		return nil, err
 	}
-	return os.Create(outputPath)
+	return nil, nil
 }
