7e5998aa6a84d4ed7c942cfe71afc7b847104af6
juliaokmenezes <julia.menezes@ccc.ufcg.edu.br>
2024-11-19 09:53:37 -0300

    🖨️  add save on jsonl

--- python/jix.py
+++ python/jix.py
@@ -41,19 +41,35 @@ def fetch_issues(project_key,jira):
     issues = jira.search_issues(f'project={project_key}',maxResults=1000)
     return issues
 
-def save_to_jsonl(issues,output_path,project_key):
+def save_to_jsonl(issues, output_path, project_key):
     json_file_path = os.path.join(output_path, f"{project_key.lower()}.jsonl")
-    os.makedirs(os.path.dirname(json_file_path), exist_ok=True)  
-    with open(json_file_path, "w") as file:
-        for issue in issues:
-            json_data = json.dumps(issue)  
-            file.write(json_data + "\n") 
+    
+    try:
+        os.makedirs(os.path.dirname(json_file_path), exist_ok=True)
+        
+        with open(json_file_path, "w", encoding="utf-8") as file:
+            for issue in issues:
+                json_data = {
+                    "id": issue.id,
+                    "key": issue.key,
+                    "fields": {
+                        "summary": issue.fields.summary,
+                        "status": {
+                            "description": issue.fields.status.description,
+                            "name": issue.fields.status.name
+                        }
+                    }
+                }
+                file.write(json.dumps(json_data, ensure_ascii=False-on) + "\n")  
+    
+    except Exception as e:
+        print(f"Error while saving to JSONL: {e}")
 
 
 
-def save_to_file(issues, output_format):
+def save_to_file(issues,output_format,project_key,output_path):
     if output_format == 'jsonl':
-        save_to_jsonl(issues)
+        save_to_jsonl(issues,output_path,project_key)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Jira Issue eXtractor")
@@ -65,3 +81,4 @@ if __name__ == "__main__":
     args = parser.parse_args()
     jira = connect_to_jira()
     issues = fetch_issues(args.project_key, jira)
+    save_to_file(issues,args.format,args.project_key,args.output_path)
